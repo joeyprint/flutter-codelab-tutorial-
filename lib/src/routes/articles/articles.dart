2da519a89e997e61c0005207b191b2615901f018
joeywasin <wasin.ww1402@gmail.com>
2022-08-05 17:15:09 +0700

    feat: integrate article item into article page

--- lib/src/routes/articles/articles.dart
+++ lib/src/routes/articles/articles.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter_app_test/src/views/components/article_item/article_item.dart';
 
 class Articles extends StatelessWidget {
+  static const String routeName = '/articles';
   const Articles({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: const Center(
-        child: Text(
-          'Articles Page',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.w600,
-          ),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            for (int i = 0; i < 10; i++)
+              const ArticleItem(
+                imageUrl: 'https://picsum.photos/200/300',
+                title: 'Lorem Ipsum',
+                description: 'Lorem description',
+                createdAt: '18 Feb 2022',
+              )
+          ],
         ),
       ),
     );
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_app_test/src/routes/articles/articles.dart';
 import 'package:flutter_app_test/src/routes/home/home.dart';
 import 'package:flutter_app_test/src/routes/generator/generator.dart';
 import 'package:flutter_app_test/src/routes/landing/landing.dart';
@@ -8,6 +9,7 @@ class Routing {
   static const String initialRoute = '/';
   static Map<String, Widget Function(BuildContext)> routes = {
     '/': (BuildContext context) => const Home(),
+    '/articles': (BuildContext context) => const Articles(),
     '/landing': (BuildContext context) => const Landing(),
     '/generator': (BuildContext context) => const Generator(),
   };
